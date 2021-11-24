f430d8c1e08b4d3fe3ca257800541900b60aa7ee
da28el <69147003+da28el@users.noreply.github.com>
2021-11-24 08:00:39 +0100

    Create raycast.js

---
+++ raycast/raycast.js
@@ -0,0 +1,134 @@
+    var vec2 = /** @class */ (function () {
+    function vec2(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.x = x;
+        this.y = y;
+    }
+    vec2.prototype.set = function (x, y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    };
+    vec2.prototype.length = function () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    };
+    vec2.prototype.str = function () {
+        return "(" + this.x + ", " + this.y + ") - |" + this.length() + "|";
+    };
+    vec2.add = function (v1, v2) {
+        return new vec2(v1.x + v2.x, v1.y + v2.y);
+    };
+    vec2.sub = function (v1, v2) {
+        return new vec2(v1.x - v2.x, v1.y - v2.y);
+    };
+    vec2.normal = function (v) {
+        return new vec2(v.x / v.length(), v.y / v.length());
+    };
+    vec2.scale = function (v, s) {
+        return new vec2(v.x * s, v.y * s);
+    };
+    vec2.fromAngle = function (theta) {
+        theta *= Math.PI / 180;
+        return new vec2(Math.cos(theta), Math.sin(theta));
+    };
+    return vec2;
+}());
+var boundry = /** @class */ (function () {
+    function boundry(v1, v2) {
+        this.p1 = v1;
+        this.p2 = v2;
+    }
+    return boundry;
+}());
+var ray = /** @class */ (function () {
+    function ray(p, d) {
+        this.pos = p;
+        this.dir = d;
+    }
+    ray.prototype.collision = function (b) {
+        var x1 = this.pos.x;
+        var y1 = this.pos.y;
+        var x2 = this.dir.x * ray.max_dist;
+        var y2 = this.dir.y * ray.max_dist;
+        var x3 = b.p1.x;
+        var y3 = b.p1.y;
+        var x4 = b.p2.x;
+        var y4 = b.p2.y;
+        var d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (d == 0)
+            return new vec2(0, 0);
+        var t = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
+        var u = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3);
+        t /= d;
+        u /= d;
+        if (u > 0 && u < 1 && t > 0 && t < 1)
+            return new vec2(x3 + u * (x4 - x3), y3 + u * (y4 - y3));
+        return vec2.scale(this.dir, ray.max_dist);
+    };
+    ray.max_dist = 1500;
+    return ray;
+}());
+var source = /** @class */ (function () {
+    function source(p) {
+        this.rays = [];
+        this.pos = p;
+        this.cast();
+    }
+    source.prototype.cast = function () {
+        for (var i = 0; i < 180; i++)
+            this.rays.push(new ray(this.pos, vec2.fromAngle(2 * i)));
+    };
+    return source;
+}());
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var width = canvas.width; // = window.innerWidth;
+var height = canvas.height; // = window.innerHeight;
+var s = new source(new vec2(width / 2, height / 2));
+var boundries = [];
+boundries.push(new boundry(new vec2(10, 70), new vec2(80, 40)));
+boundries.push(new boundry(new vec2(15, 60), new vec2(280, 70)));
+boundries.push(new boundry(new vec2(40, 100), new vec2(70, 280)));
+boundries.push(new boundry(new vec2(280, 400), new vec2(400, 270)));
+function paint() {
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, width, height);
+    s.cast();
+    for (var j = 0; j < boundries.length; j++) {
+        ctx.strokeStyle = 'rgb(255,0,0)';
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(boundries[j].p1.x, boundries[j].p1.y);
+        ctx.lineTo(boundries[j].p2.x, boundries[j].p2.y);
+        ctx.stroke();
+    }
+    for (var i = 0; i < s.rays.length; i++) {
+        var nearest = vec2.scale(new vec2(2, 2), ray.max_dist + 1);
+        for (var j = 0; j < boundries.length; j++) {
+            var z = s.rays[i].collision(boundries[j]);
+            if (vec2.sub(s.pos, z).length() < vec2.sub(s.pos, nearest).length())
+                nearest = z;
+        }
+        ctx.strokeStyle = 'rgb(255,255,255)';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(s.rays[i].pos.x, s.rays[i].pos.y);
+        ctx.lineTo(nearest.x, nearest.y);
+        ctx.stroke();
+    }
+}
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+function moveSource(evt) {
+    var pos = getMousePos(canvas, evt);
+    s.pos.x = pos.x;
+    s.pos.y = pos.y;
+    paint();
+}
+paint();
