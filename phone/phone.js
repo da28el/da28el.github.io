cd0725a7e1279e4bcadfa22c0f0b57dfb822bc19
da28el <69147003+da28el@users.noreply.github.com>
2021-11-27 02:47:52 +0100

    Create phone.js

---
+++ phone/phone.js
@@ -0,0 +1,6 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var width = canvas.width = window.innerWidth * 0.8;
+var height = canvas.height = window.innerHeight * 0.8;
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(0, 0, width, height);
